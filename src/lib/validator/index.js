ab443db149866555f85ea9bfd6a46a2c9d2e6d20
chin <a5566qq2581@gmail.com>
2022-11-01 22:12:36 +0800

    feat: load any json file in IG dir
    - For single FHIR profile that not bind to IG

--- src/lib/validator/index.js
+++ src/lib/validator/index.js
@@ -1,8 +1,22 @@
 const path = require("path");
+const fs = require("fs");
+
 const validator = require("node-java-fhir-validator")(
-    path.normalize(
-        path.join(__dirname, "./igs")
-    )
+  path.normalize(path.join(__dirname, "./igs"))
 );
+
+const fhirProfileFiles = fs.readdirSync(path.join(__dirname, "./igs"));
+fhirProfileFiles.forEach(async (file) => {
+  let extName = path.extname(file);
+  if (extName === ".json") {
+    let resource = fs.readFileSync(path.join(__dirname, "./igs", file), "utf8");
+    try {
+      await validator.loadProfile(resource);
+    } catch(e) {
+      console.error(e);
+    }
+    
+  }
+});
 
 module.exports = validator;
